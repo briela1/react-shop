9e82b080504e51164ef90409e9478728cdcf7b63
briela1 <98447320+briela1@users.noreply.github.com>
2023-01-18 11:20:39 -0500

    atomic design

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/NotFound.scss';
+
+const NotFound = () => {
+    return (
+        <div className="login">
+            <div className="form-container">
+                <div className="email-image">
+                    <img src="./Logos/404 Error.svg" alt="404" />
+                </div>
+                <h1 className="title">Oops!</h1>
+                <p className="subtitle">We can't seem to find the page you're looking for.</p>
+                <button className="primary-button login-button">Go back</button>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
