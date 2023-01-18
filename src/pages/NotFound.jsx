59c9562f06eb6c99c8273d3a01e7b90c764997b7
briela1 <98447320+briela1@users.noreply.github.com>
2023-01-18 13:29:14 -0500

    alias img

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import '../styles/NotFound.scss';
+import '@styles/NotFound.scss';
+
+import error from '@logos/404 Error.svg';
 
 const NotFound = () => {
     return (
         <div className="login">
             <div className="form-container">
                 <div className="email-image">
-                    <img src="./Logos/404 Error.svg" alt="404" />
+                    <img src={error} alt="404" />
                 </div>
                 <h1 className="title">Oops!</h1>
                 <p className="subtitle">We can't seem to find the page you're looking for.</p>
